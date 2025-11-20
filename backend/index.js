e52a0b71b00b48e786b203e47ab6dd5d9e851caf
Abanoub Shazly <154024309+sharlyops@users.noreply.github.com>
2025-11-20 20:00:13 +0200

    Update index.jsfor the iu

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(express.json());
+app.use(express..json());
 app.use(cors()); // Adjust CORS as needed for production
 
 // POST /api/contact endpoint
